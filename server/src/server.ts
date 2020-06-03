2b85e4175bf82d2032c9b5659324e1792cd6e4e0
Pedro Santos <phsantosti@gmail.com>
2020-06-03 11:10:32 -0300

    Initial Commit

---
+++ server/src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+    console.log('Listagem de Usuários');
+    response.json([
+        'Diego', 'Cleiton', 'Robson'
+    ]);
+});
+
+app.listen(3333);
